be73b73ba0fb8d8124aad8b8f2ddeac99e574008
Doug Lasa <27441098+DFYT42@users.noreply.github.com>
2019-04-18 18:53:29 -0700

    Update nagios-client-configuration.sh

--- nagios-client-configuration.sh
+++ nagios-client-configuration.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
-yum -y install nrpe nagios-nrpe-server nagios-plugins
+#yum -y install nrpe nagios-nrpe-server nagios-plugins
+
+yum install -y nagios-nrpe-server nagios-plugins nrpe nagios-plugins-load nagios-plugins-ping nagios-plugins-disk nagios-plugins-http nagios-plugins-procs wget
+# BUG:https://osric.com/chris/accidental-developer/2016/12/missing-nagios-plugins-in-centos-7/ (nrpe plugins have been packaged seperately and don't install with nagios-plugins-all)
+# BUG #2 https://cloudwafer.com/blog/installing-nagios-agent-npre-on-centos/ (the nrpe config is commented out and checks are not defined)
+# Use sed statments to uncomment NRPE config and add the appropiate flags
+# add in command[check_mem]=/usr/lib64/nagios/plugins/check_mem.sh
+# Install custom mem monitor
+wget -O /usr/lib64/nagios/plugins/check_mem.sh https://raw.githubusercontent.com/nic-instruction/NTI-320/master/nagios/check_mem.sh
 
 systemctl enable nrpe
 systemctl start nrpe
