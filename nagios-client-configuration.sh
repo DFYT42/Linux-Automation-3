0d6ce0853faf3d361b0298cad9a5374bc023bcd6
Doug Lasa <27441098+DFYT42@users.noreply.github.com>
2019-04-18 21:30:05 -0700

    Update nagios-client-configuration.sh
    a

--- nagios-client-configuration.sh
+++ nagios-client-configuration.sh
@@ -18,13 +18,13 @@ systemctl enable nrpe
 systemctl start nrpe
 ###There's another line over in troubleshooting to pull over here###
 #sed #command[check_mem]=/usr/lib64/nagios/plugins/custom_check_mem -n $ARG1$,command[check_mem]=/usr/lib64/nagios/plugins/check_mem.sh -w 80 -c 90,g' /etc/nagios/nrpe.cfg 
-### MISC SYSTEM METRICS ###
+#####MISC SYSTEM METRICS#####
 #command[check_users]=/usr/lib64/nagios/plugins/check_users $ARG1$
 #command[check_load]=/usr/lib64/nagios/plugins/check_load $ARG1$
 #command[check_disk]=/usr/lib64/nagios/plugins/check_disk $ARG1$
 #command[check_swap]=/usr/lib64/nagios/plugins/check_swap $ARG1$
 #command[check_cpu_stats]=/usr/lib64/nagios/plugins/check_cpu_stats.sh $ARG1$
-### MISC SYSTEM METRICS ###
+#####MISC SYSTEM METRICS#####
 command[check_users]=/usr/lib64/nagios/plugins/check_users -w 5 -c 10
 command[check_load]=/usr/lib64/nagios/plugins/check_load -w 15,10,5 -c 30,25,20
 command[check_disk]=/usr/lib64/nagios/plugins/check_disk -w 20% -c 10%
