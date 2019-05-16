02dc92c07c652c57b1c8a8b391ec8041fd924e32
Doug Lasa <27441098+DFYT42@users.noreply.github.com>
2019-05-15 17:53:27 -0700

    Create for_loop_for_nrpe_install.sh

---
+++ for_loop_for_nrpe_install.sh
@@ -0,0 +1,3 @@
+for servername in $( gcloud compute instances list | awk '{print $1}' | sed "1 d" | grep -v nagios-a );  do 
+    gcloud compute ssh --zone us-west2-a g42dfyt@$servername --command='sudo yum -y install wget && sudo wget https://raw.githubusercontent.com/nic-instruction/NTI-320/master/nagios/nrpe_install.sh && sudo bash nrpe_install.sh'
+done
