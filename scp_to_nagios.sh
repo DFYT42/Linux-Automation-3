d69648cb4a2b062ca2941e1000cc149bdd037f84
Doug Lasa <27441098+DFYT42@users.noreply.github.com>
2019-05-15 18:19:31 -0700

    Update scp_to_nagios.sh

--- scp_to_nagios.sh
+++ scp_to_nagios.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 bash generate_config.sh $1 $2
 gcloud compute scp --zone us-west2-a $1.cfg g42dfyt@nagios-a:/etc/nagios/servers
-# Note: I had to add user nicolebade to group nagios using usermod -a -G nagios g42dfyt
+# Note: 
+#Nagios server code near bottom: I had to add user nicolebade to group nagios on nagios server using usermod -a -G nagios g42dfyt
 # I also had to chmod 777 /etc/nagios/servers
 gcloud compute ssh --zone us-west2-a g42dfyt@nagios-a --command='sudo /usr/sbin/nagios -v /etc/nagios/nagios.cfg'
