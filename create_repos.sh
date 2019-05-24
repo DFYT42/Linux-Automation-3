81ed022830171b54174ad23bd6d1519a30a2bb3e
Doug Lasa <27441098+DFYT42@users.noreply.github.com>
2019-05-23 20:04:16 -0700

    Update create_repos.sh

--- create_repos.sh
+++ create_repos.sh
@@ -5,14 +5,19 @@ yum -y install createrepo                                                       
 mkdir -p /repos/centos/7/extras/x86_64/Packages/                                   # Make your repo dir
 
 ##copy from build
-##suggested to copy from build to NFS and create share for other servers
-##create the rpm and then copy to home directory of build server
+##suggested to copy from build server to NFS and create share for other servers
+##or can cp to home, scp to cloud cli, and scp to repo-server
+##create the rpm on build server and then copy to home directory of build server
 ##cp /root/rpmbuild/RPMS/x86_64/helloworld-0.1-1.el7.x86_64.rpm /home/g42dfyt/
-##chown it to be used by you
+##chown it to be owned by you
 ##chown g42dfyt /home/g42dfyt/helloworld-0.1-1.el7.x86_64.rpm
 ##go to google cloud command line
-
-cp helloworld-0.1-1.el7.centos.x86_64.rpm /repos/centos/7/extras/x86_64/Packages   # Replace 'helloworld*' with your package
+##gcloud compute scp g42dfyt@build-a:/home/g42dfyt/helloworld-0.1-1.el7.x86_64.rpm .
+#Did you mean zone [us-west1-a] for instance: [build-a] (Y/n)?  n
+##gcloud compute scp helloworld-0.1-1.el7.x86_64.rpm g42dfyt@repo-serv:/home/g42
+##dfyt
+##Did you mean zone [us-west1-a] for instance: [repo-serv] (Y/n)?  n
+cp helloworld-0.1-1.el7.x86_64.rpm /repos/centos/7/extras/x86_64/Packages   # Replace 'helloworld*' with your package
 
 createrepo --update /repos/centos/7/extras/x86_64/Packages/                        # Update after every change
 
@@ -21,14 +26,16 @@ createrepo --update /repos/centos/7/extras/x86_64/Packages/                     
 
 yum -y install httpd                                                              # Now install apache so you can serve your repo over the web
 
+##disable selinux
 setenforce 0
 
 systemctl enable httpd
 
 systemctl start httpd
-
+##cretae symbolic link between repo centos and web centos
+##/var/www/ centos points to repos
 ln -s  /repos/centos /var/www/html/centos                                         # Link your repos in
-
+##always copy config files to .bak before editting
 cp /etc/httpd/conf/httpd.conf /etc/httpd/conf/httpd.conf.bak                      # Make a copy of our origonal httpd.conf
 
 sed -i '144i     Options All' /etc/httpd/conf/httpd.conf                          # Configure apache
@@ -37,6 +44,7 @@ sed -i '146i     DirectoryIndex disabled' /etc/httpd/conf/httpd.conf
 
 sed -i 's/^/#/' /etc/httpd/conf.d/welcome.conf                                    # Disables the defualt welcome page in the recommended way
 
+##-R makes recursive chown
 chown -R apache:apache /repos/
 
 systemctl restart httpd
