44d77477d9cdd659de33ca8fc0087c95bbf93eeb
Doug Lasa <27441098+DFYT42@users.noreply.github.com>
2019-05-30 19:04:29 -0700

    Create final_nrpe_plugin.sh

---
+++ final_nrpe_plugin.sh
@@ -0,0 +1,25 @@
+
+#!/bin/bash
+# This is a script meant to let us test NRPE
+# plugins we write
+#http://tldp.org/LDP/abs/html/comparison-ops.html
+
+versionp=$(python --version) 
+versionp3=$(python3 --version)
+
+if [ $versionp3 != "bash: python3: command not found" && $versionp != "bash: python: command not found" ]; then
+    echo "Overachiever! You have both Python3 and Python installed"
+    exit 0;
+    
+  elif [ $versionp3 == "bash: python3: command not found" && $versionp == "bash: python: command not found" ]; then
+    echo "You do not have Python3 or Python installed"
+    exit 2;
+    
+  elif [ $versionp3 == "bash: python3: command not found" && $versionp != "bash: python: command not found" ] ; then
+    echo "You do not have Python3 installed but do have Python installed"
+    exit 1;
+    
+else
+   echo "VERSION:UNKNOWN"
+   exit 3;
+fi
