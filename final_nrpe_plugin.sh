98bee10601f3616a90734028ce641117168a2c02
Doug Lasa <27441098+DFYT42@users.noreply.github.com>
2019-06-05 16:33:10 -0700

    Update final_nrpe_plugin.sh

--- final_nrpe_plugin.sh
+++ final_nrpe_plugin.sh
@@ -1,7 +1,8 @@
-
 #!/bin/bash
-# This is a script meant to let us test NRPE
-# plugins we write
+# JBeach
+# This is a script meant to check if you have python installed
+# If it is installed, it will tell you if you have one or multiple versions, and your default version
+# If it is not installed, it will tell you and then install for you
 #http://tldp.org/LDP/abs/html/comparison-ops.html
 
 versionp=$(ls -l /usr/bin/ | grep python* | wc -l)
@@ -10,17 +11,22 @@ if [ "$versionp" -gt 1 ]; then
     echo "You have multiple versions of Python installed"
     echo "Your default version is:"
     python --version
+    echo "OK"
     exit 0;
 
   elif [ $versionp -lt 1 ]; then
-    echo "You do not have Python installed"
+    echo "You do not have Python installed but it will be installed now"
     yum -y install python
     #apt-get -y install python
+    echo "CRITICAL"
     exit 2;
 
   elif [ $versionp = 1 ]; then
     echo "You have one version of Python installed"
     python --version
+    echo "OK"
+    exit 0;
+    
 else
    echo "VERSION:UNKNOWN"
    exit 3;
