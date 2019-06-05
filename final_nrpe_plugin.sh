679b01dc642afd9798c5f41fe6367077c2040b60
Doug Lasa <27441098+DFYT42@users.noreply.github.com>
2019-06-05 16:05:11 -0700

    Update final_nrpe_plugin.sh

--- final_nrpe_plugin.sh
+++ final_nrpe_plugin.sh
@@ -4,17 +4,22 @@
 # plugins we write
 #http://tldp.org/LDP/abs/html/comparison-ops.html
 
-versionp=$(ls -l | grep python | wc -l)
+versionp=$(ls -l /usr/bin/ | grep python* | wc -l)
 
-if [ "$versionp" -gt 0]; then
-    echo "You have Python installed"
+if [ "$versionp" -gt 1 ]; then
+    echo "You have multiple versions of Python installed"
+    echo "Your default version is:"
+    python --version
     exit 0;
-    
-  elif [ $versionp -e 0]; then
+
+  elif [ $versionp -lt 1 ]; then
     echo "You do not have Python installed"
-    yum install python
+    apt-get -y install python
     exit 2;
-    
+
+  elif [ $versionp = 1 ]; then
+    echo "You have one version of Python installed"
+    python --version
 else
    echo "VERSION:UNKNOWN"
    exit 3;
