17818ffbb45be81c959e19a8c09f30dd565705b2
Doug Lasa <27441098+DFYT42@users.noreply.github.com>
2019-06-06 19:58:44 -0700

    Update for_loop_for_nrpe_install.sh

--- for_loop_for_nrpe_install.sh
+++ for_loop_for_nrpe_install.sh
@@ -1,3 +1,3 @@
-for servername in $( gcloud compute instances list | awk '{print $1}' | sed "1 d" | grep -v nagios-a );  do 
-    gcloud compute ssh --zone us-west2-a g42dfyt@$servername --command='sudo yum -y install wget && sudo wget https://raw.githubusercontent.com/DFYT42/Linux-Automation-3/master/nagios-client-configuration.sh && sudo bash nagios-client-configuration.sh'
+for servername in $( gcloud compute instances list | awk '{print $1}' | sed "1 d" | grep -v nti320-final-nagios-server );  do 
+    gcloud compute ssh --zone us-west1-b g42dfyt@$servername --command='sudo yum -y install wget && sudo wget https://raw.githubusercontent.com/DFYT42/Linux-Automation-3/master/nagios-client-configuration.sh && sudo bash nagios-client-configuration.sh'
 done
