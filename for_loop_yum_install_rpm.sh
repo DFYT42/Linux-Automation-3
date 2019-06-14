0df6194939d734074e714261fe2a28a20812ff73
Doug Lasa <27441098+DFYT42@users.noreply.github.com>
2019-06-13 21:22:46 -0700

    Update for_loop_yum_install_rpm.sh

--- for_loop_yum_install_rpm.sh
+++ for_loop_yum_install_rpm.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
-for servername in $( gcloud compute instances list | awk '{print $1}' | sed "1 d" | grep -v nti320-final-repo-server );  do 
+#for servername in $( gcloud compute instances list | awk '{print $1}' | sed "1 d" | grep -v nti320-final-repo-server );  do 
    
-    gcloud compute ssh g42dfyt@$servername --zone us-west1-b --strict-host-key-checking='no' --command='sudo yum -y install wget && sudo wget https://raw.githubusercontent.com/DFYT42/Linux-Automation-3/master/add_yum_repo.sh && sudo bash add_yum_repo.sh';
-
-done
-
-#for servername in $(gcloud compute instances list | awk '{print $1}' | sed "1 d" | grep -v $repo_server); do 
-
-#gcloud compute ssh cchang30@$servername --zone us-west1-b --strict-host-key-checking='no' --command='sudo yum -y install wget && sudo wget https://raw.githubusercontent.com/chuanisawesome/NTI-320/master/lab4_repo/add-yum.sh && sudo bash add-yum.sh';
+#    gcloud compute ssh g42dfyt@$servername --zone us-west1-b --strict-host-key-checking='no' --command='sudo yum -y install wget && sudo wget https://raw.githubusercontent.com/DFYT42/Linux-Automation-3/master/add_yum_repo.sh && sudo bash add_yum_repo.sh';
 
 #done
+
+for servername in $(gcloud compute instances list | awk '{print $1}' | sed "1 d" | grep -v $nti320_final_repo_server); do 
+   gcloud compute ssh g42dfyt@$servername --zone us-west1-b --strict-host-key-checking='no' --command='sudo yum -y install wget && sudo wget https://raw.githubusercontent.com/DFYT42/Linux-Automation-3/master/add-yum_repo.sh && sudo bash add_yum_repo.sh';
+done
