0fc9662be5e4d80ee95fd9771c0c962481454d7c
Doug Lasa <27441098+DFYT42@users.noreply.github.com>
2019-06-13 21:16:57 -0700

    Update add_yum_repo.sh

--- add_yum_repo.sh
+++ add_yum_repo.sh
@@ -3,14 +3,15 @@
 #add to existing repos (ls /etc/yum.repos.d)
 
 ##repo server ip
-ip="10.138.0.200"                                                    # Please replace with your repo server IP
-
+##ip="10.138.0.200"                                                    # Please replace with your repo server IP
+repo_server="nti320-final-repo-server"
+repo_ip=$(gcloud compute instances list | grep $repo_server | awk '{ print $4 }' | tail -1)
 #vim /etc/yum.repos.d/epel.repo
-echo '[nti-320]
+echo '[nti-320-plugins]
 name=Extra Packages for Centos from NTI-320 7 - $basearch
 #baseurl=http://download.fedoraproject.org/pub/epel/7/$basearch <- example epel repo
 # Note, this is putting repodata at packages instead of 7 and our path is a hack around that.
-baseurl=http://$ip/centos/7/extras/x86_64/Packages/
+baseurl=http://$repo_ip/centos/7/extras/x86_64/Packages/
 enabled=1
 gpgcheck=0
 ' >> /etc/yum.repos.d/NTI-320.repo                                  
