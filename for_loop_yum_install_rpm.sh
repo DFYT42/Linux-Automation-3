4b2425ca865c2484fd5e5783e273c4e89dd566ee
Doug Lasa <27441098+DFYT42@users.noreply.github.com>
2019-06-13 21:12:50 -0700

    Update for_loop_yum_install_rpm.sh

--- for_loop_yum_install_rpm.sh
+++ for_loop_yum_install_rpm.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
 for servername in $( gcloud compute instances list | awk '{print $1}' | sed "1 d" | grep -v nti320-final-repo-server );  do 
-    gcloud compute ssh --zone us-west1-b g42dfyt@$servername --command='sudo yum -y install wget && sudo wget https://raw.githubusercontent.com/DFYT42/Linux-Automation-3/master/add_yum_repo.sh && sudo bash add_yum_repo.sh';
+   
+    gcloud compute ssh g42dfyt@$servername --zone us-west1-b --strict-host-key-checking='no' --command='sudo yum -y install wget && sudo wget https://raw.githubusercontent.com/DFYT42/Linux-Automation-3/master/add_yum_repo.sh && sudo bash add_yum_repo.sh';
+
 done
+
+#for servername in $(gcloud compute instances list | awk '{print $1}' | sed "1 d" | grep -v $repo_server); do 
+
+#gcloud compute ssh cchang30@$servername --zone us-west1-b --strict-host-key-checking='no' --command='sudo yum -y install wget && sudo wget https://raw.githubusercontent.com/chuanisawesome/NTI-320/master/lab4_repo/add-yum.sh && sudo bash add-yum.sh';
+
+#done
