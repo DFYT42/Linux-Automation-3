b0d08b09a7f21f7a330aa404accd4eae1bdf3980
Doug Lasa <27441098+DFYT42@users.noreply.github.com>
2019-06-13 20:56:23 -0700

    Update for_loop_yum_install_rpm.sh

--- for_loop_yum_install_rpm.sh
+++ for_loop_yum_install_rpm.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 for servername in $( gcloud compute instances list | awk '{print $1}' | sed "1 d" | grep -v nti320-final-repo-server );  do 
     gcloud compute ssh --zone us-west1-b g42dfyt@$servername --command='sudo yum -y install wget && sudo wget https://raw.githubusercontent.com/DFYT42/Linux-Automation-3/master/add_yum_repo.sh && sudo bash add_yum_repo.sh'
 done
