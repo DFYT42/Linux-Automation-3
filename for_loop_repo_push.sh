818ed8ab088c812e8e356123dba0c42ab8df7837
Doug Lasa <27441098+DFYT42@users.noreply.github.com>
2019-06-13 20:42:45 -0700

    Update for_loop_repo_push.sh

--- for_loop_repo_push.sh
+++ for_loop_repo_push.sh
@@ -1,3 +1,3 @@
-for servername in $( gcloud compute instances list );  do 
+for servername in $( gcloud compute instances list | awk '{print $1}' | sed "1 d");  do 
     gcloud compute scp nti-320-plugins-0.1-1.el7.x86_64.rpm g42dfyt@$servername:/home/g42dfyt/
 done
